821fed3c509643b195e435257c5999f1b5763816
roman.mykhailiuk <roman.mykhailiuk@eleks.com>
2020-08-25 23:34:30 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>HTTP Lesson</h1>
+    <div>
+      <label>Username</label>
+      <input type="text" v-model="user.userName">
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="user.email">
+    </div>
+    <button @click="submitData">Submit</button>
+    <button @click="getUsers">Get users</button>
+    <button @click="deleteUser">Delete user</button>
+    <button @click="updateUser">Update user</button>
+    <hr>
+    <ul>
+      <li v-for="(user, i) in users" :key="i">
+        {{user}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      user: {
+        userName: '',
+        email: ''
+      },
+      users: [],
+    }
+  },
+
+  methods: {
+    submitData() {
+      // this.$http.post('http://localhost/users', this.user);
+      // this.$http.post('https://http-lesson-vue.firebaseio.com/users.json', this.user)
+      // .then((res) => {
+      //   console.log(res);
+      // }, error => {
+      //   console.log(error)
+      // })
+      this.$http.post(`users.json`, this.user)
+          .then((res) => {
+            console.log(res);
+          });
+    },
+
+    getUsers() {
+      this.$http.get('users.json')
+      .then((res) => {
+        console.log(res, 'RESPONSE');
+        return res.json()
+      }).then((res) => {
+        console.log(res, 'JSON');
+        Object.values(res).forEach(u => this.users.push(u));
+      })
+    },
+
+    deleteUser() {
+      // this.$http.delete(`https://http-lesson-vue.firebaseio.com/${id}`)
+      this.$http.delete('users.json')
+      .then((res) => {
+        console.log(res, 'DELETE');
+      });
+    },
+
+    updateUser() {
+      this.$http.put('-MFamzam49v-kBz1r_kw.json', {
+        userName: 'name',
+        email: 'email',
+        id: '33',
+        job: 'seniour'
+      }).then((res) => {
+        console.log(res, 'UPDATE');
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://http-lesson-vue.firebaseio.com';
 
 new Vue({
   render: h => h(App),
